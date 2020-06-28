cbed3dc8d3279fa0e543a7def22e583b026f86ee
andscost <andrecosta888@gmail.com>
2020-06-28 16:31:21 -0300

    atualizei o codico

--- RPG andre.py
+++ RPG andre.py
@@ -9,8 +9,8 @@ class Personagem:
         dano = min(max(randint(0, self.hp) - randint(0, inimigo.hp), 0),inimigo.hp)
         inimigo.hp = inimigo.hp - dano
         if dano == 0:
-            print "%s esquivou do ataque de %s." % (inimigo.nome, self.nome)
-        else: print "%s feriu %s!" % (self.nome, inimigo.nome)
+            print (f"{inimigo.nome} esquivou do ataque de {self.nome}.")
+        else: print (f"{self.nome} feriu {inimigo.nome}!")
         return inimigo.hp <= 0
 
 class Monstro(Personagem):
@@ -28,56 +28,56 @@ class jogador (Personagem):
         self.hp_max=10
         self.nome = nome
     def sair (self):
-      print "%s nao pode achar uma saida e morre de fome.\nR.I.P." % self.nome
+      print (f"{self.nome} nao pode achar uma saida e morre de fome.\nR.I.P.")
       self.hp = 0
-    def ajuda (self): print Comandos.keys()
-    def status (self):print " saude de %s : %d/%d" % (self.nome, self.hp, self.hp_max)
+    def ajuda (self): print (Comandos.keys())
+    def status (self):print (f" saude de {self.nome} : {self.hp}/{self.hp_max}")
     def cansado (self):
-       print "%s se sente cansado" % self.nome
+       print (f"{self.nome} se sente cansado")
        self.hp = max(1, self.hp - 1)
     def descansar (self):
         if self.estado != 'normal':
-            print "%s nao pode descansar agora!" % self.nome
+            print (f"{self.nome} nao pode descansar agora!")
             self.ataque_inimigo()
         else:
-            print "%s descansa..." % self.nome
+            print (f"{self.nome} descansa...")
             if randint(0,1):
                 self.inimigo = Monstro(self)
-                print "%s e rudamente acordado por %s!" % (self.nome, self.inimigo.nome)
+                print (f"{self.nome} e rudamente acordado por {self.inimigo.nome}!")
                 self.estado = 'luta'
                 self.ataque_inimigo()
             else:
                 if self.hp < self.hp_max:
                     self.hp = self.hp + 1
-                else: print "%s dormiu demais." % self.nome; self.hp = self.hp - 1
+                else: print (f"{self.nome}dormiu demais."); self.hp = self.hp - 1
     def explorar (self):
         if self.estado != 'normal':
-            print "%s esta muito ocupado agora." % self.nome
+            print (f"{self.nome} esta muito ocupado agora.")
             self.ataque_inimigo()
         else:
-            print "%s explora uma passagem assombrosa." % self.nome
+            print (f"{self.nome} explora uma passagem assombrosa.")
             if randint(0,1):
                 self.inimigo = Monstro(self)
-                print "%s encontra %s!" % (self.nome, self.inimigo.nome)
+                print (f"{self.nome} encontra {self.inimigo.nome}!")
                 self.estado = 'luta'
             else:
                 if randint(0,1): self.cansado()
     def ataque (self):
         if self.estado !='luta':
-            print "%s soca o ar, aparentimente sem resultados." %self.nome
+            print (f"{self.nome} soca o ar, aparentimente sem resultados.")
             self.cansado()
         else:
             if self.dano(self.inimigo):
-                print "%s executa %s!" % (self.nome, self.inimigo.nome)
+                print (f"{self.nome} executa {self.inimigo.nome}!")
                 self.inimigo = None
                 self.estado = 'normal'
                 if randint(0, self.hp) < 10:
                     self.hp_max = self.hp_max + 1
-                    print "%s se sente mais forte!" %self.nome
+                    print (f"{self.nome} se sente mais forte!")
             else: self.ataque_inimigo()
     def ataque_inimigo(self):
         if self.inimigo.dano(self):
-            print "%s foi assassino por %s !!!\nR.I.P" %(self.nome, self.inimigo.nome)
+            print (f"{self.nome} foi assassino por {self.inimigo.nome} !!!\nR.I.P")
 
 Comandos = {
     "sair": jogador.sair,
@@ -90,23 +90,23 @@ Comandos = {
 restart = True    
     
 while restart :                   
-    p = jogador(raw_input("qual sera o nome de seu personagem?  "))
+    p = jogador(input("qual sera o nome de seu personagem?  "))
     
-    print "digite ajuda para uma lista de comandos"
+    print ("digite ajuda para uma lista de comandos")
     p.ajuda()
-    print "%s entra numa cavena escura procurando por uma aventura" % p.nome 
+    print (f"{p.nome}s entra numa cavena escura procurando por uma aventura") 
 
     while p.hp > 0 :
-        controle = raw_input("> ")
+        controle = input("> ")
         comandoencontrar = False
         for comando in Comandos.keys():
             if comando == controle:
                 Comandos[controle](p)
                 comandoencontrar = True
         if not comandoencontrar:
-            print "nao foi possivel executar esse comando, favor digitar outra vez"
+            print ("nao foi possivel executar esse comando, favor digitar outra vez")
 
-    pergunta = raw_input("GAME OVER, se deseja recomesar digite = 1, se nao digite 0 ")
+    pergunta = input("GAME OVER, se deseja recomesar digite = 1, se nao digite 0 ")
     if pergunta == '0':
         restart = False
     else:
